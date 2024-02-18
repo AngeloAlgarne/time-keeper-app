f738bc1b6221db09fc8809188b6b4cb0b3272bec
geloiam <74101193+AngeloAlgarne@users.noreply.github.com>
2024-02-19 02:34:24 +0800

    Dockerize the entire app

--- backend/timetracker/models.py
+++ backend/timetracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Project(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
