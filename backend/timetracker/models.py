7e30460e9998fbb3d8160a940c03f756caf878de
geloiam <74101193+AngeloAlgarne@users.noreply.github.com>
2024-02-19 00:30:08 +0800

    Added user field to project

--- backend/timetracker/models.py
+++ backend/timetracker/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from django.utils import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
